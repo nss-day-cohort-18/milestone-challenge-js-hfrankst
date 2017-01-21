c0e3db19c4ef6327f291b4c63b5cc56768f3798b
Harper Frankstone <Harper Frankstone>
2017-01-21 15:33:09 -0600

    set up and connected the functions, now working on building the tree

--- main.js
+++ main.js
@@ -1,42 +1,42 @@
-//Use parseInt() to ensure that the height value is always an integer
-//Use height value -1 on each line to properly space the characters out, creating the tree shape, take advantage of the .repeat() method
-//The for loop needs to run as many iterations as is the height value
-//
+/* Notes and Thoughts:
+Use parseInt() to ensure that the height value is always an integer
+Use height value -1 on each line to properly space the characters out, creating the tree shape, take advantage of the .repeat() method
+The for loop needs to run as many iterations as is the height value
 
-//Use event listeners to track the 'Grow your Tree' button and the keypress of the enter key, if the cursor is in the input field
-//If either field is left blank, display alert prompting the user to provide input
+Use event listeners to track the 'Grow your Tree' button and the keypress of the enter key, if the cursor is in the input field
+If either field is left blank, display alert prompting the user to provide input
+*/
 
-
-var character = document.getElementById("treeChar");
-var height = document.getElementById("height");
-// var empty = " ";
+var character = document.getElementById("inputChar");
+var height = document.getElementById("inputHeight");
+var empty = " ";
 
 var charObject = {};
-//take the height input and force it to be an integer
-//height.addEventListener("keyup", tree).parseInt();
 
-console.log(height);	
-console.log(character);
+	
 
 function tree() {
-	for (i = 0; i < charArray.length; i++) {
+	// for each (value in charObject) {
 		
-	}
+	// }
+	console.log(charObject.)
 
 
 	return
 };
 
 function captureCharacter() {
+//grab the character that is used to build the tree
 	charObject.character = character.value;
 	return
 };
 
 function captureHeight() {
-	charObject.height = height.value;
+//take the height input and force it to be an integer
+	charObject.height = parseInt(height.value);
 	return
 };
 
-inputChar.addEventListener('keyup', captureCharacter);
-inputHeight.addEventListener('keyup', captureHeight);
+inputChar.addEventListener('change', captureCharacter);
+inputHeight.addEventListener('change', captureHeight);
 
